e0220209a55b2cd931a1e3a6c439574019260f14
Spesho-clei <jepkoechnancy56@gmail.com>
2024-03-14 09:24:06 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Takes a float number as input and returns its floor as an integer.
+    """
+    return math.floor(n)
